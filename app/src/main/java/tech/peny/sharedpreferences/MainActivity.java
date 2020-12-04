07f0dd10136730b4327e8a89c478ae5ef482e56b
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-04 13:26:42 +0000

    Working with shared preferences
    Add data to shared preferences, getting data from shared preferences, serializing and arraylist, deserializing an arraylist

--- app/src/main/java/tech/peny/sharedpreferences/MainActivity.java
+++ app/src/main/java/tech/peny/sharedpreferences/MainActivity.java
@@ -2,7 +2,14 @@ package tech.peny.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("tech.peny.sharedpreferences", Context.MODE_PRIVATE);
+//        putting information into shared preferences
+        sharedPreferences.edit().putString("username","nick").apply();
+
+//        getting information from shared preferences
+        String  username = sharedPreferences.getString("username","");
+        Log.i("this is the username ->",username);
+
+//        saving an array of things
+        ArrayList<String> friends = new ArrayList<String>();
+
+        friends.add("Fido");
+        friends.add("Sarah");
+        friends.add("Jones");
+//        Serializing an array list
+        try {
+            sharedPreferences.edit().putString("friends",ObjectSerializer.serialize(friends)).apply();
+            Log.i("friends",ObjectSerializer.serialize(friends));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//      deserializing an array list
+        ArrayList<String> newFriends = new ArrayList<>();
+
+        try {
+            newFriends =(ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("friends", ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.i("new Friends", newFriends.toString());
     }
 }
